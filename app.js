bc42e2b988a85f8106b6daf230f36b0a18bdc898
kunwar-11 <kunwarrishabh70@gmail.com>
2020-12-01 01:49:06 +0530

    added functionality (javascript)

---
+++ app.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('textarea');
+var btnTranslate = document.querySelector('#btn-translate');
+var output = document.querySelector('.output');
+
+var url = "https://api.funtranslations.com/translate/sith.json";
+
+function urlBuilder(inputText) {
+    return url + "?text=" + inputText;
+}
+
+
+function clickHandler() {
+    //fetch the translation from API
+    fetch(urlBuilder(input.value))
+    .then(res => res.json())
+    .then(json => {
+        //show the translated output
+        output.innerText = json.contents.translated;
+        
+    })
+    .catch(err => alert(err +"occured please try after sometime"))
+    
+}
+
+btnTranslate.addEventListener('click', clickHandler);
